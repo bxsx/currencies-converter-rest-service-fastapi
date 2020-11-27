3f74125d84fbe5e6cdaeac2e09c8151378b6804c
Bart Skowron <233652+bxsx@users.noreply.github.com>
2020-11-27 20:40:22 +0100

    Initialize Redis on startup and shutdown

--- app/main.py
+++ app/main.py
@@ -1,3 +1,4 @@
+import fastapi_plugins
 from fastapi import FastAPI
 
 from . import api
@@ -5,3 +6,14 @@ from .settings import config
 
 app = FastAPI()
 app.include_router(api.router, prefix=f"{config.API_PREFIX}/convert", tags=["currency"])
+
+
+@app.on_event("startup")
+async def on_startup() -> None:
+    await fastapi_plugins.redis_plugin.init_app(app, config=config)
+    await fastapi_plugins.redis_plugin.init()
+
+
+@app.on_event("shutdown")
+async def on_shutdown() -> None:
+    await fastapi_plugins.redis_plugin.terminate()
