afa5cba6d4b341b7cb03bb4076ee776f8536cbf7
Bart Skowron <233652+bxsx@users.noreply.github.com>
2020-11-29 20:19:00 +0100

    Add helpers: pair_iter()
    Pair currencies iterator over permutation of all supported currencies

--- tests/helpers/supported_currencies.py
+++ tests/helpers/supported_currencies.py
@@ -1,4 +1,5 @@
-from typing import Generator, Optional
+import itertools
+from typing import Generator, Optional, Tuple
 
 from app.schemas import SupportedCurrencyModel
 
@@ -7,4 +8,10 @@ def currency_iter(limit: Optional[int] = None) -> Generator[str, None, None]:
     if limit is None:
         limit = len(SupportedCurrencyModel)
 
-    return (c.value for c, _ in zip(SupportedCurrencyModel, range(limit)))
+    return (c for c, _ in zip(SupportedCurrencyModel, range(limit)))
+
+
+def pair_iter() -> Generator[
+    Tuple[SupportedCurrencyModel, SupportedCurrencyModel], None, None
+]:
+    yield from itertools.permutations(list(SupportedCurrencyModel), 2)
--- tests/helpers/test/test_supported_currencies.py
+++ tests/helpers/test/test_supported_currencies.py
@@ -1,3 +1,7 @@
+import math
+
+from app.schemas import SupportedCurrencyModel
+
 from .. import supported_currencies as sc
 
 
@@ -10,3 +14,17 @@ def test_get_limited_currency_iterator() -> None:
 
     for i in range(maximum_value):
         assert len(list(sc.currency_iter(limit=i))) == i
+
+
+def test_iterate_over_currencies_pair():
+    n = len(SupportedCurrencyModel)
+    possible_combinations = math.perm(n, 2)
+
+    returned_pairs = []
+    for pair in sc.pair_iter():
+        assert pair[0] in SupportedCurrencyModel
+        assert pair[1] in SupportedCurrencyModel
+        assert pair not in returned_pairs
+        returned_pairs.append(pair)
+
+    assert len(returned_pairs) == possible_combinations
